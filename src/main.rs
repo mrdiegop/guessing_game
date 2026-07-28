8ec7e2fc0bd5fc001567264066e53aeb0ec44853
mrdiegop <diegoaperezguzman@gmail.com>
2026-07-27 22:21:56 -0400

    se actualiza nuevamente

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,18 @@ fn main() {
         
         println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)  //metodo para leer una entrada de usuario
-        .expect("Failed to read line"); // en caso que falla el metodo read_line, se imprime el mensaje de error
+        io::stdin()
+            .read_line(&mut guess)  //metodo para leer una entrada de usuario
+            .expect("Failed to read line"); // en caso que falla el metodo read_line, se imprime el mensaje de error
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // convierte la variable inpu user a un i32
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // convierte la variable inpu user a un i32
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {   // realiza una comparacion entre el numero secreto y el numero ingresado por el usuario
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {   // realiza una comparacion entre el numero secreto y el numero ingresado por el usuario
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
     }
 
 
